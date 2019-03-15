f3a131d85b746977cfb868c629f1a69a85ba301b
Gunter Hillenbrand <gunterhillenbrand@geniesolutions.com.au>
2019-03-15 10:33:51 +1000

    changes

--- src/games/TicTacToe/index.js
+++ src/games/TicTacToe/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import {split} from 'fusion-react';
-
-const ClientRenderedOnly = split({
-  defer: true,
-  load: () => import('./component'),
-  LoadingComponent: () => <div></div>,
-  ErrorComponent: () => <div>Error</div>,
-});
-
-export default ClientRenderedOnly;
+export { default } from './component';
--- src/pages/ticTacToePage.js
+++ src/pages/ticTacToePage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'fusion-plugin-react-router';
 import FusionStyle from '../components/fusionStyle';
 import Center from '../components/center';
-import TicTacToe from '../games/PacMan';
+import TicTacToe from '../games/TicTacToe';
 
 const TicTacToePage = () => {
   return (
     <>
-      <Link to="/">Home</Link>
+      <Link to="/"><h1>Home</h1></Link>
       <Center direction="column">
         <FusionStyle>Tic Tac Toe</FusionStyle>
         <TicTacToe/>
